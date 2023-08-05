63420799826c72032fc8b09adf631d47eb4de096
Vn-Boy <0939899928za.com@gmail.com>
2023-08-05 20:02:48 +0700

    ...

--- server/src/api/v1/controllers/services/CheckSoscial.ts
+++ server/src/api/v1/controllers/services/CheckSoscial.ts
@@ -1,51 +1,54 @@
-import { Request, Response, NextFunction } from 'express';
-import { OAuth2Client } from 'google-auth-library';
-import userModel from '../../models/User_management/userModel';
-require('dotenv').config();
+import { Request, Response, NextFunction } from "express";
+import { OAuth2Client } from "google-auth-library";
+import userModel from "../../models/User_management/userModel";
+require("dotenv").config();
 const oAuth2Client = new OAuth2Client(
-	process.env.CLIENT_ID,
-	process.env.CLIENT_SECRET,
-	'postmessage'
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  "postmessage"
 );
-import expressAsyncHandler from 'express-async-handler';
+import expressAsyncHandler from "express-async-handler";
 
 export const middlwareSocialLogin = expressAsyncHandler(
-	async (req: Request, res: Response, next: NextFunction) => {
-		if (req.query.isSocialLogin) {
-			let userInfo;
-			if (req.query.google) {
-				const ticket = await oAuth2Client.getToken(req.body.code);
-				userInfo = await fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
-					headers: {
-						Authorization: `Bearer ${ticket.tokens.access_token}`,
-					},
-				});
-			} else if (req.query.facebook) {
-				userInfo = await fetch(
-					`https://graph.facebook.com/me?fields=id,name,email&access_token=${req.body.accessToken}`
-				);
-			}
-			const data = await userInfo!.json();
-			const user = await userModel.findOne({ email: data.email });
-			if (user) {
-				if (user.isSocialConnect) {
-					req.body.username = user.username;
-					req.body.isSocialLogin = true;
-					return next();
-				} else {
-					res.json({
-						isExisted: true,
-						user: data,
-						isSocialConnect: false,
-					});
-				}
-			} else {
-				res.json({
-					isExisted: false,
-					user: data,
-					isSocialLogin: true,
-				});
-			}
-		} else return next();
-	}
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (req.query.isSocialLogin) {
+      let userInfo;
+      if (req.query.google) {
+        const ticket = await oAuth2Client.getToken(req.body.code);
+        userInfo = await fetch(
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          {
+            headers: {
+              Authorization: `Bearer ${ticket.tokens.access_token}`,
+            },
+          }
+        );
+      } else if (req.query.facebook) {
+        userInfo = await fetch(
+          `https://graph.facebook.com/me?fields=id,name,email&access_token=${req.body.accessToken}`
+        );
+      }
+      const data = await userInfo!.json();
+      const user = await userModel.findOne({ email: data.email });
+      if (user) {
+        if (user.isSocialConnect) {
+          req.body.username = user.username;
+          req.body.isSocialLogin = true;
+          return next();
+        } else {
+          res.json({
+            isExisted: true,
+            user: data,
+            isSocialConnect: false,
+          });
+        }
+      } else {
+        res.json({
+          isExisted: false,
+          user: data,
+          isSocialLogin: true,
+        });
+      }
+    } else return next();
+  }
 );
